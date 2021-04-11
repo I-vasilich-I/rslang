24fcbaeaff691407665bfe21beebd2eef1ae7adc
Nimlu <snesterov@gmail.com>
2021-04-11 21:19:59 +0300

    fix: make it working again RL-43

--- src/helpers/soundHelper.ts
+++ src/helpers/soundHelper.ts
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Howl } from 'howler';
+// import React from 'react';
+import { Howl, Howler } from 'howler';
 import { BACKEND_API_URL } from '../constants/constants';
-import { useTypedSelector } from '../hooks/useTypeSelector';
-import { useActions } from '../hooks/useActions';
+// import { useTypedSelector } from '../hooks/useTypeSelector';
+// import { useActions } from '../hooks/useActions';
 
-export const soundHandler = function (audio: string, audioMeaning: string, audioExample: string) {
-    const { sound: reduxSound } = useTypedSelector((state) => state.sound);
-    const { setSound } = useActions();
+export const soundHandler = (audio: string, audioMeaning: string, audioExample: string) => {
+    // const { sound: reduxSound } = useTypedSelector((state) => state.sound);
+    // const { setSound } = useActions();
 
     const sound = new Howl({
         src: [`${BACKEND_API_URL}${audio}`],
@@ -18,36 +18,37 @@ export const soundHandler = function (audio: string, audioMeaning: string, audio
         src: [`${BACKEND_API_URL}${audioExample}`],
     });
 
-    if (reduxSound > 0) {
-        sound.stop(reduxSound);
-        soundMeaning.stop(reduxSound);
-        soundExample.stop(reduxSound);
-    }
+    // if (reduxSound > 0) {
+    //     sound.stop(reduxSound);
+    //     soundMeaning.stop(reduxSound);
+    //     soundExample.stop(reduxSound);
+    // }
 
     sound.on('end', () => {
-        setSound(soundMeaning.play());
+        soundMeaning.play();
     });
     soundMeaning.on('end', () => {
-        setSound(soundExample.play());
+        soundExample.play();
     });
     soundExample.on('end', () => {
         soundExample.stop();
-        setSound(-1);
+        //setSound(-1);
     });
 
-    return function () {
-        if (sound.playing() || reduxSound !== -1) {
-            //sound.stop();
-            sound.stop(reduxSound);
-        } else if (soundMeaning.playing() || reduxSound !== -1) {
-            // soundMeaning.stop();
-            soundMeaning.stop(reduxSound);
-        } else if (soundExample.playing() || reduxSound !== -1) {
-            // soundExample.stop();
-            soundMeaning.stop(reduxSound);
+    return () => {
+        if (sound.playing()) {
+            sound.stop();
+            //sound.stop(reduxSound);
+        } else if (soundMeaning.playing()) {
+            soundMeaning.stop();
+            //soundMeaning.stop(reduxSound);
+        } else if (soundExample.playing()) {
+            soundExample.stop();
+            //soundMeaning.stop(reduxSound);
         } else {
-            setSound(sound.play());
+            Howler.unload;
+            sound.play();
+            //setSound(sound.play());
         }
     };
-    //playHandler();
 };
